43fa2ac978bb34e4e5a2954925a646f4cb877427
AltyFox <bartat404@gmail.com>
2024-05-07 18:17:57 -0500

    Bare scaffolding built.  Time to end this project for the day

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
+import { createSignal, onMount } from 'solid-js'
 import './App.css'
+
+
 
 function App() {
   const [count, setCount] = createSignal(0)
+  const [modsData, setModsData] = createSignal({"0": []})
+  const [coreModsData, setCoreModsData] = createSignal({})
+  onMount(async () => {
+    const modsResponse = await fetch('./assets/mods.json')
+    const modsJson = await modsResponse.json()
+    const reorganizedModsData = {}
+    for (const version in modsJson) {
+      reorganizedModsData[version] = {}
+      for (const mod of modsJson[version]) {
+        const { id, ...modData } = mod
+        if (!reorganizedModsData[version][id]) {
+          reorganizedModsData[version][id] = []
+        }
+        reorganizedModsData[version][id].push(modData)
+      }
+    }
+    const sortedReorganizedModsData = Object.keys(reorganizedModsData)
+      .sort()
+      .reverse()
+      .reduce((sortedData, version) => {
+        if (version != "undefined") {
+          sortedData[version] = reorganizedModsData[version];
+        }
+        return sortedData;
+      }, {});
+
+    setModsData(sortedReorganizedModsData);
+
+    const coreModsResponse = await fetch('./assets/core_mods.json')
+    const coreModsJson = await coreModsResponse.json()
+    setCoreModsData(coreModsJson)
+  })
+
+  function showMods(modData) {
+    let modsForThisVersion = modsData()[modData.target.value];
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
+    <p>Select a version below</p>
+      <select onChange={showMods} id="game_version" name="game_version">
+      <For each={Object.keys(modsData())}> 
+        {(version) =>
+          <option value={version}>{version.split('_')[0]}</option>
+        }
+      </For>
+      </select>
+
     </>
   )
 }
 
 export default App
+
